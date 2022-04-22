caf0c75d269463af8a1f540615b3f4a1bd9764ff
davidv-laptop <david.valaczkai@epfl.ch>
2022-04-22 22:00:37 +0200

    Mark old program as deprecated

--- cmd/carrier/carrier.go
+++ cmd/carrier/carrier.go
@@ -1,19 +1,13 @@
 package main
 
-import (
-	"github.com/OerllydSaethwr/carrier/pkg/carrier"
-	"os"
-	"strconv"
-	"sync"
-)
-
 func main() {
-	wg := &sync.WaitGroup{}
-	front := os.Args[1]
-	port, _ := strconv.Atoi(os.Args[2])
-
-	c := carrier.NewCarrier(wg, front, port)
-	c.Start()
-
-	wg.Wait()
+	//TODO deprecated
+	//wg := &sync.WaitGroup{}
+	//front := os.Args[1]
+	//port, _ := strconv.Atoi(os.Args[2])
+	//
+	//c := carrier.NewCarrier(wg, front, port)
+	//c.Start()
+	//
+	//wg.Wait()
 }
